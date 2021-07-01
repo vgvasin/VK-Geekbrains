afa25b0cc0d8cb9da81615099924e8ab81aa9d24
VasinVG <vasin229@gmail.com>
2021-07-01 21:33:15 +0300

    lesson-1(upd)

--- VK-Geekbrains/VK-Geekbrains/LoginFormController.swift
+++ VK-Geekbrains/VK-Geekbrains/LoginFormController.swift
@@ -40,33 +40,42 @@ class LoginFormController: UIViewController {
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        // Получаем текст логина
+        let login = loginInput.text!
+        // Получаем текст-пароль
+        let password = passwordInput.text!
+        // Проверяем, верны ли они
+        if login == "admin" && password == "123456" { print("успешная авторизация")
+        } else {
+        print("неуспешная авторизация")
+        }
     }
     
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        // Проверяем данные
-        let checkResult = checkUserData()
-        // Если данные не верны, покажем ошибку
-        if !checkResult { showLoginError()
-        }
-        // Вернем результат
-        return checkResult }
-    func checkUserData() -> Bool {
-        guard let login = loginInput.text,
-              let password = passwordInput.text else { return false }
-        if login == "admin" && password == "123456" { return true
-        } else {
-            return false
-        } }
-    func showLoginError() {
-        // Создаем контроллер
-        let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
-        // Создаем кнопку для UIAlertController
-        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        // Добавляем кнопку на UIAlertController
-        alter.addAction(action)
-        // Показываем UIAlertController
-        present(alter, animated: true, completion: nil)
-    }
+//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//        // Проверяем данные
+//        let checkResult = checkUserData()
+//        // Если данные не верны, покажем ошибку
+//        if !checkResult { showLoginError()
+//        }
+//        // Вернем результат
+//        return checkResult }
+//    func checkUserData() -> Bool {
+//        guard let login = loginInput.text,
+//              let password = passwordInput.text else { return false }
+//        if login == "admin" && password == "123456" { return true
+//        } else {
+//            return false
+//        } }
+//    func showLoginError() {
+//        // Создаем контроллер
+//        let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+//        // Создаем кнопку для UIAlertController
+//        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//        // Добавляем кнопку на UIAlertController
+//        alter.addAction(action)
+//        // Показываем UIAlertController
+//        present(alter, animated: true, completion: nil)
+//    }
     
     @objc func hideKeyboard() { self.scrollView?.endEditing(true)
     }
